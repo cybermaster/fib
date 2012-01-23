35f264ce66bf43e4013a5371477bb1d36e262d3b
Bill Zhao <tauneutrino@gmail.com>
2012-01-22 17:48:59 -0800

    init commit for file fib1.py; the first commit for proj

---
+++ fib1.py
@@ -0,0 +1,34 @@
+
+__author__ = 'billz'
+
+
+#def fib1(num):
+#    if num == 0:
+#        return 0
+#    if num == 1:
+#        return 1
+#    return fib1(num-1) + fib1(num-2)
+#
+#def fib2(num):
+#    if num == 0: return 0
+#
+#    num+=1
+#    fibarry = [0] * num
+#    fibarry[0] = 0
+#    fibarry[1] = 1
+#
+#    for i in range(2, num):
+#        fibarry[i] = fibarry[i-1] + fibarry[i-2]
+##        print fibarry[i]
+#
+#    return fibarry[num-1]
+#
+#while True:
+#    print "Fibonacci Number (enter a number; enter 0 to quit)"
+#    userNumber = input("Enter a number: ")
+#    if userNumber == 0: break
+#    print "fib2(", userNumber, ") = ", fib2(userNumber)
+#
+#    #print "fib1(", usernumber, ") = ", fib1(usernumber)
+#
+#
